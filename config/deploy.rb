3a70dfaab069e9c666b0f0137df4f4c7323ebfa2
gen-g-gen <59825257+gen-g-gen@users.noreply.github.com>
2020-03-12 13:04:53 +0900

    edit again

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,4 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.12.0"
-
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
 
 # config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
@@ -12,7 +8,7 @@ lock '3.12.0'
 set :application, 'lineBot'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:gen-g-gen/lineBot.git'
+set :repo_url, 'git@github.com:gen-g-gen/lineBot.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
